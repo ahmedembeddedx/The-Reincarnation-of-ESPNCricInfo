6c5af06a0e459b90636c5bae755d067754f7187c
Ahmed Abdullah <137221373+ahmedembeddedx@users.noreply.github.com>
2024-03-05 09:54:23 -0800

    Add Pages, Improved Session Authentication

--- Website/scripts.js
+++ Website/scripts.js
@@ -1,16 +1,83 @@
+// Function to check credentials
 function checkCredentials() {
     var username = document.getElementById('username').value;
     var password = document.getElementById('password').value;
 
-    if (username == 'Admin' && password == 'Admin123') {
+    if (username === 'Admin' && password === 'Admin123') {
         document.getElementById('loginStatus').innerText = 'AdminLogin:Allowed';
+        sessionStorage.setItem('isAdminLoggedIn', true);
+        sessionStorage.setItem('isUserLoggedIn', true);
+        window.location.href = "admin.html";
     }
-    else if (username == 'User' && password == 'User123')
-    {
+    else if (username === 'User' && password === 'User123') {
         document.getElementById('loginStatus').innerText = 'UserLogin:Allowed';
+        sessionStorage.setItem('isUserLoggedIn', true);
+        window.location.href = "updates.html";
     }
-    else
-    {
+    else {
         document.getElementById('loginStatus').innerText = 'Error';
+    }
+}
+
+function toPlayers() {
+    if (sessionStorage.getItem('isUserLoggedIn') !== 'true' && sessionStorage.getItem('isAdminLoggedIn') !== 'true') {
+        window.location.href = "login.html";
+    }
+    else {
+        window.location.href = "players.html";
+    }
+}
+
+function toHome() {
+    if (sessionStorage.getItem('isUserLoggedIn') !== 'true' && sessionStorage.getItem('isAdminLoggedIn') !== 'true') {
+        window.location.href = "login.html";
+    }
+    else {
+        window.location.href = "updates.html";
+    }
+}
+
+function toTeams() {
+    if (sessionStorage.getItem('isUserLoggedIn') !== 'true' && sessionStorage.getItem('isAdminLoggedIn') !== 'true') {
+        window.location.href = "login.html";
+    }
+    else {
+        window.location.href = "teams.html";
+    }
+}
+
+function toMatches() {
+    if (sessionStorage.getItem('isUserLoggedIn') !== 'true' && sessionStorage.getItem('isAdminLoggedIn') !== 'true') {
+        window.location.href = "login.html";
+    }
+    else {
+        window.location.href = "matches.html";
+    }
+}
+
+function toSeries() {
+    if (sessionStorage.getItem('isUserLoggedIn') !== 'true' && sessionStorage.getItem('isAdminLoggedIn') !== 'true') {
+        window.location.href = "login.html";
+    }
+    else {
+        window.location.href = "series.html";
+    }
+}
+
+function toUpdates() {
+    if (sessionStorage.getItem('isUserLoggedIn') !== 'true' && sessionStorage.getItem('isAdminLoggedIn') !== 'true') {
+        window.location.href = "login.html";
+    }
+    else {
+        window.location.href = "updates.html";
+    }
+}
+
+function toAdmin() {
+    if (sessionStorage.getItem('isAdminLoggedIn') !== 'true') {
+        window.location.href = "login.html";
+    }
+    else {
+        window.location.href = "admin.html";
     }
 }
