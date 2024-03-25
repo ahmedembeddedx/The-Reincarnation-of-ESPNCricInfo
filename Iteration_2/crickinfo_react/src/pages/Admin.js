9753c3b0f7b3a3b4b5991e7a64a79a5e03498ef6
Ahmad Abdullah <business.ahmadabdullah@gmail.com>
2024-03-25 16:46:02 +0500

    Add Packages

---
+++ Iteration_2/crickinfo_react/src/pages/Admin.js
@@ -0,0 +1,63 @@
+import React from 'react'
+
+export default function Admin() {
+  return (
+    <div>
+        <h3>Admin Option</h3>
+        <div id="FormTable" style={{width: '50%'}}>
+            {/* <div id="" className="displaytext" style={{ color: '#282828', fontFamily: 'monospace' }}>Loading...</div>
+             */}
+
+            <table>
+              <tr>
+                <th>Add</th>
+                <th>Update</th>
+                <th>Delete</th>
+              </tr>
+              <tr>
+                <td><button className='b4' id="addTeam">Add Team</button></td>
+                <td><button className='b4' id="updateTeam">Update Team</button></td>
+                <td><button className='b5' style={{backgroundColor :'#cc0000'}}  id="deleteTeam">Delete Team</button></td>
+              </tr>
+              <tr>  
+                <td><button className='b4' id="addPlayer">Add Player</button></td>
+                <td><button className='b4' id="updatePlayer">Update Player</button></td>
+                <td><button className='b5' style={{backgroundColor :'#cc0000'}}  id="deletePlayer">Delete Player</button></td>
+              </tr>
+              <tr>
+                <td><button className='b4' id="addSeries">Add Series</button></td>
+                <td><button className='b4' id="updateSeries">Update Series</button></td>
+                <td><button className='b5' style={{backgroundColor :'#cc0000'}}  id="deleteSeries">Delete Series</button></td>
+              </tr>
+              <tr>
+                <td><button className='b4' id="addMatch">Add Match</button></td>
+                <td><button className='b4' id="updateMatch">Update Match</button></td>
+                <td><button className='b5' style={{backgroundColor :'#cc0000'}} id="deleteMatch">Delete Match</button></td>
+              </tr>
+              <tr>
+                <td><button className='b4' id="addNews">Add News</button></td>
+                <td><button className='b4' id="updateNews">Update News</button></td>
+                <td><button className='b5' style={{backgroundColor :'#cc0000'}} id="deleteNews">Delete News</button></td>
+              </tr>
+              <tr>
+                <td><button className='b5' style={{backgroundColor :'#0000ff'}} id='addAdmin'>Add Admin</button></td>
+                <td><button className='b5' style={{backgroundColor :'#0000ff'}} id='updateAdmin'>Update Admin</button></td>
+                <td><button className='b5' style={{backgroundColor :'#cc0000'}} id='deleteAdmin'>Delete Admin</button></td>
+              </tr>
+
+
+            </table>
+
+
+        </div>
+        <br/>
+        <br/>
+        <hr/>
+        <footer>
+            <p>ESPNCricInfo Reincarnated</p>
+            <p>Copyright 2024. All Rights Reserved.</p>
+            <a href="https://github.com/ahmedembeddedx"><img src="https://cdn-icons-png.freepik.com/512/919/919847.png?ga=GA1.1.1925836337.1709750745&" alt="" height="40"/></a>
+        </footer>
+    </div>
+  )
+}
---
+++ Iteration_2/crickinfo_react/src/pages/Players.js
@@ -0,0 +1,69 @@
+import React from 'react'
+
+export default function Players() {
+  return (
+    <div>
+        <h3>Player Data</h3>
+        <input type="text" id="search" placeholder="Find Player..." />
+        <button id="searchButton">Search</button>
+        <div id="FormTable">
+            
+            <table>
+                <tr>
+                    <th>Player Name</th>
+                    <th>Player ID</th>
+                    <th>Player Role</th>
+                    <th>Player Team</th>
+                    <th>Player Matches</th>
+                    <th>Player Runs</th>
+                    <th>Player Wickets</th>
+                    <th>Player Catches</th>
+                    <th>Player Stumpings</th>
+                </tr>
+                <tr>
+                    <td>Babar Azam</td>
+                    <td>1</td> 
+                    <td>Batsman</td>
+                    <td>Pakistan</td>
+                    <td>100</td>
+                    <td>5000</td>
+                    <td>0</td>
+                    <td>50</td>
+                    <td>0</td>
+                </tr>
+                <tr>
+                    <td>Virat Kohli</td>
+                    <td>2</td> 
+                    <td>Batsman</td>
+                    <td>India</td>
+                    <td>150</td>
+                    <td>8000</td>
+                    <td>0</td>
+                    <td>100</td>
+                    <td>0</td>
+                </tr>
+                <tr>
+                    <td>Steve Smith</td>
+                    <td>3</td> 
+                    <td>Batsman</td>
+                    <td>Australia</td>
+                    <td>120</td>
+                    <td>6000</td>
+                    <td>0</td>
+                    <td>70</td>
+                    <td>0</td>
+                </tr>
+            </table>
+            
+        </div>
+        <br/>
+        <br/>
+        <hr/>
+        <footer>
+            <p>ESPNCricInfo Reincarnated</p>
+            <p>Copyright 2024. All Rights Reserved.</p>
+            <a href="https://github.com/ahmedembeddedx"><img src="https://cdn-icons-png.freepik.com/512/919/919847.png?ga=GA1.1.1925836337.1709750745&" alt="" height="40"/></a>
+        </footer>
+    </div>
+  )
+}
